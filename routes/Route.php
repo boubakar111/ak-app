417a23cbc9c1f54396250d6f8dee2e36b43e57f5
akdouche boubakar <akdouche_b@outlook.fr>
2024-04-22 17:54:47 +0200

    structure du projet et la config de base

---
+++ routes/Route.php
@@ -0,0 +1,41 @@
+<?php
+namespace Routes;
+
+use Database\DBConnection;
+
+class Route
+{
+// variable qui stock le path et les actions.
+    public $path;
+    public $action;
+    public $matches;
+
+    public function __construct(string $path, string $action)
+    {
+        $this->path = trim($path, '/');
+        $this->action = $action;
+    }
+
+    public function matches(string $url): bool
+    {
+
+        $path = preg_replace('#:([a-zA-Z0-9]+)#', '([^/]+)', $this->path);
+        //  path to compare.
+        $pathToMatche = "#^$path$#";
+
+        if (preg_match($pathToMatche, $url, $matches)) {
+            $this->matches = $matches;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function execute()
+    {
+        $params = explode('@', $this->action);
+        $controller = new $params[0](new DBConnection());
+        $method = $params[1];
+        return isset($this->matches[1]) ? $controller->$method($this->matches[1]) : $controller->$method();
+    }
+}
---
+++ src/Exception/NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Exception;
+
+use Exception;
+use Throwable;
+
+class NotFoundException extends Exception
+{
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
